ca96468cfad0ed8ce1fa40d8004d0e9691f0b031
Omar Gaber <omargaber956@gmail.com>
2022-05-01 16:17:58 +0200

    Added tests and linting config.

---
+++ w3/src/tests/accountSpec.ts
@@ -0,0 +1,19 @@
+import supertest from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+
+describe('Account test suite', () => {
+  const path = '/api/accounts';
+  it('Performs GET /', async (): Promise<void> => {
+    const response: supertest.Response = await request.get(path);
+    expect(response.status).toBe(200);
+  });
+
+  it('Last name missing', async (): Promise<void> => {
+    const response: supertest.Response = await request.get(
+      `${path}?first_name=Mo`
+    );
+    expect(response.text).toBe('Last Name Missing.');
+  });
+});
