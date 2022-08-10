92ed101c3764c7e0bcc8906e05fd2944abdd2bc9
Omar Gaber <omargaber956@gmail.com>
2022-08-10 20:49:27 +0200

    Added database.ts file.

---
+++ july_22/w6/src/models/users.ts
@@ -0,0 +1,31 @@
+import client from "../database";
+import bcrypt from 'bcrypt';
+
+export type User = {
+    id?: number;
+    first_name: string;
+    last_name: string;
+    balance: number;
+    email: string;
+    password: string;
+}
+
+
+
+
+export class UserStore {
+    async index(): Promise<User[]> {
+    try {
+        const conn = await client.connect()
+        const query = `SELECT * FROM users`
+        const result = await conn.query(query)
+        conn.release()
+        return result.rows
+    }
+    catch (err) {
+        throw new Error(`Cannot get users ${err}`)
+    }
+
+}
+}
+
--- july_22/w6/src/server.ts
+++ july_22/w6/src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import user_routes from './handlers/users';
+// import user_routes from './handlers/users';
 
 const app: express.Application = express()
 const address: string = "0.0.0.0:3000"
@@ -16,7 +16,7 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
-user_routes(app)
+// user_routes(app)
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`)
