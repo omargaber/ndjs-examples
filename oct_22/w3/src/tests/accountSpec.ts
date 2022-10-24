e1ecca63af01ecea72f1b37a343277d18bf3c7ee
Omar <omargaber956@gmail.com>
2022-10-24 19:54:32 +0200

    Removed test cases.

--- oct_22/w3/src/tests/accountSpec.ts
+++ oct_22/w3/src/tests/accountSpec.ts
@@ -4,20 +4,5 @@ import app from '../index';
 const request = supertest(app);
 
 describe('Account test suite', () => {
-  const path = '/api/accounts';
-  beforeEach(()=> {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  });
-  
-  it('Performs GET /', async (): Promise<void> => {
-    const response: supertest.Response = await request.get(path);
-    expect(response.status).toBe(200);
-  });
 
-  it('Last name missing', async (): Promise<void> => {
-    const response: supertest.Response = await request.get(
-      `${path}?first_name=Mo`
-    );
-    expect(response.text).toBe('Last Name Missing.');
-  });
 });
